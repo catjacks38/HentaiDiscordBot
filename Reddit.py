acc58e18697b2219fc9eba92fde7c91ec709053f
catjacks38 <jbranchfie@gmail.com>
2021-08-09 09:33:49 -0400

    Added refresh command
    Added ".reddit refresh <cache>" command
    Added comments/documentation
    Other minor code changes

--- Reddit.py
+++ Reddit.py
@@ -2,22 +2,27 @@ import praw
 import pickle
 import time
 
-
+# A class to store the time and a list image urls for the caches, because storing it all in a list caused an error. T_T
 class Submissions:
     def __init__(self, time, submissions):
         self.time = time
         self.submissions = submissions
 
-
+# A class for scrapping the images off of r/hentai
 class ImageScrapper:
+    # How long until automatic cache refresh (stored in seconds)
     topCacheRefreshTime = 600
     hotCacheRefreshTime = 210
 
+    # The limit on how many posts to grab
     hotLimit = 200
     topLimit = 200
 
     def __init__(self, clientID, clientSecret):
+        # Creates bot
         self.__bot = praw.Reddit(user_agent="Image Scrapper Thing (by u/catjacks38)", client_id=clientID, client_secret=clientSecret)
+
+        # Tries to read the cache files--sets them to False if they do not exist or can't be read
         try:
             with open("top.post", "rb") as topFile:
                 self.__top = topFile.read()
@@ -33,6 +38,8 @@ class ImageScrapper:
     def RefreshCache(self, section):
         images = []
 
+        # Tries to get the submissions, and returns -1 if it fails
+        # If section is not a valid section, the function returns -2
         try:
             if section == "top":
                 posts = self.__bot.subreddit("hentai").top("day", limit=self.topLimit)
@@ -46,9 +53,11 @@ class ImageScrapper:
         for post in posts:
             url = post.url
 
+            # Only appends the URL if it is an image URL
             if url[:18] == "https://i.redd.it/" or url[:20] == "https://i.imgur.com/":
                 images.append(url)
 
+        # Saves the posts and the time to the correct cache file
         if section == "top":
             with open("top.post", "wb") as topFile:
                 pickle.dump(Submissions(int(time.time()), images), topFile)
@@ -66,16 +75,21 @@ class ImageScrapper:
         return 0
 
     def Get(self, section):
+
+        # If section is not a valid section, the function returns -2
         if section == "top":
             if self.__top:
                 submissions = pickle.loads(self.__top)
 
+                # Checks if the automatic refresh time has not been reached
+                # Refreshes cache if it has
                 if int(time.time()) - submissions.time < self.topCacheRefreshTime:
                     return submissions.submissions
 
             returnValue = self.RefreshCache("top")
-            print(self.__top)
 
+            # Loads the top Submission object, and returns the submissions variable if returnValue is 0
+            # If returnValue is not zero, the function will return returnValue
             if returnValue == 0:
                 return pickle.loads(self.__top).submissions
             else:
@@ -85,11 +99,15 @@ class ImageScrapper:
             if self.__hot:
                 submissions = pickle.loads(self.__hot)
 
+                # Checks if the automatic refresh time has not been reached
+                # Refreshes cache if it has
                 if int(time.time()) - submissions.time < self.hotCacheRefreshTime:
                     return submissions.submissions
 
             returnValue = self.RefreshCache("hot")
 
+            # Loads the hot Submission object, and returns the submissions variable if returnValue is 0
+            # If returnValue is not zero, the function will return returnValue
             if returnValue == 0:
                 return pickle.loads(self.__hot).submissions
             else:
--- bot.py
+++ bot.py
@@ -14,6 +14,8 @@ parser.add_argument("--redditSecret", "-s", help="Your reddit client secret.")
 
 args = parser.parse_args()
 
+# Attempts to read "options.cfg"
+# If reading "options.cfg" is not successful, it will print the error, and exit the program with the code of -1
 if args.discordToken and args.redditID and args.redditSecret:
     with open("options.cfg", "wb") as configFile:
         pickle.dump([args.discordToken, args.redditID, args.redditSecret], configFile)
@@ -34,6 +36,7 @@ else:
 imageScrapperReddit = Reddit.ImageScrapper(options[1], options[2])
 
 
+# Prints a success message once the bot is ready
 @bot.event
 async def on_ready():
     print("lol bot is ready")
@@ -41,25 +44,59 @@ async def on_ready():
 
 @bot.command()
 async def reddit(ctx, *, args):
-    if args == "top":
+    parsedArgs = args.split(" ")
+
+    # Checks if the first argument is valid
+    # Sends back error if the argument is invalid
+    if parsedArgs[0] == "top":
         submissions = imageScrapperReddit.Get("top")
 
+        # Checks to make sure the Get function returns a list
+        # Prints error message if it doesn't
         if isinstance(submissions, int):
             print(f"Error! Function returned {submissions}")
         else:
             await ctx.send(random.choice(submissions))
 
-    elif args == "hot":
+    elif parsedArgs[0] == "hot":
         submissions = imageScrapperReddit.Get("hot")
 
+        # Checks to make sure the Get function returns a list
+        # Prints error message if it doesn't
         if isinstance(submissions, int):
             print(f"Error! Function returned {submissions}")
         else:
             await ctx.send(random.choice(submissions))
+    elif parsedArgs[0] == "refresh":
+        await ctx.send("```Refreshing the cache...```")
+
+        # Checks to make sure there is a second argument
+        # Sends back an error message if there isn't
+        try:
+            returnValue = imageScrapperReddit.RefreshCache(parsedArgs[1])
+            print(f"Attempting to refresh the {parsedArgs[1]} cache...")
+
+            # Checks to make sure returnValue is 0
+            # If returnValue is -1, there was an error trying to get the posts
+            # If returnValue is -2, the second argument is not a valid cache
+            if returnValue == 0:
+                await ctx.send("```Finished refreshing the cache!```")
+                print("pog it all went well, and cache was refreshed")
+            elif returnValue == -1:
+                await ctx.send("```There was an error while trying to retrieve the posts!```")
+                print("yikes there was an error when trying to get the posts")
+            elif returnValue == -2:
+                await ctx.send(f"```\"{parsedArgs[1]}\" is not a valid cache!\nSupported options:\n- top\n- hot```")
+                print("smh the user never supplied a valid cache")
+        except:
+            await ctx.send("```A cache argument was never supplied!```")
+            print("smh user never even supplied a cache argument")
     else:
-        await ctx.send(f"\"{args}\" is not a supported argument of the command \".reddit\".")
+        await ctx.send(f"```\"{parsedArgs[0]}\" is not a supported argument of the command \".reddit\".```")
 
 
+# Tries to use the discord token
+# If there is an error, the script exits with code -1
 try:
     bot.run(options[0])
 except:
